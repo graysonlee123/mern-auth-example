bf29a19e376715bf38b480cb8ea2589c52defc9f
graysonlee123 <graysonleegantek@gmail.com>
2019-10-19 17:34:25 -0400

    adds login and register components

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,25 +1,25 @@
-import React, { component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Navbar extends Component {
-    render() {
-        return (
-            <div className="navbar-fixed">
-                <nav className="z-depth-0">
-                    <Link
-                        to="/"
-                        style={{
-                            fontFamily: "monospace"
-                        }}
-                        className="col s5 brand-logo center black-text"
-                    >
-                        <i className="material-icons">code</i>
-                        MERN
-                    </Link>
-                </nav>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="navbar-fixed">
+        <nav className="z-depth-0">
+          <Link
+            to="/"
+            style={{
+              fontFamily: "monospace"
+            }}
+            className="col s5 brand-logo center black-text"
+          >
+            <i className="material-icons">code</i>
+            MERN
+          </Link>
+        </nav>
+      </div>
+    );
+  }
 }
 
 export default Navbar;
