bb12ed241d6ed9e41d460da8ffd483fc7e8db193
graysonlee123 <graysonleegantek@gmail.com>
2019-10-19 12:56:37 -0400

    pulls in requirements and routes to server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require('./routes/api/users');
 
 const app = express();
 
@@ -20,6 +23,15 @@ mongoose
     )
     .then(() => console.log('MongoDB succesfully connected.'))
     .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require('./config/passport')(passport);
+
+// Routes
+app.use('/api/users', users);
 
 const PORT = process.env.PORT || 8000;
 
