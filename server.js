b75efe2d50986fe80f5b4c85df1185c16eab54c3
graysonlee123 <graysonleegantek@gmail.com>
2019-10-19 14:17:52 -0400

    adds react in client folder. small edits to server and package.json

--- server.js
+++ server.js
@@ -7,11 +7,8 @@ const users = require('./routes/api/users');
 
 const app = express();
 
-app.use(
-    bodyParser.urlencoded({
-        extended: false
-    })
-);
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const db = require('./config/keys').mongoURI;
