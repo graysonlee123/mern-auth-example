fae8992e74df8125bbcdf1c2b412219cec454b66
graysonlee123 <graysonleegantek@gmail.com>
2019-10-19 18:22:46 -0400

    sets up redux biolderplate

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { throws } from 'assert';
 
 class Register extends Component {
     constructor() {
---
+++ client/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combinedReducers, combineReducers } from 'redux';
+import authReducer from './authReducer';
+import errorReducer from './errorReducer';
+
+export default combineReducers({
+    auth: authReducer,
+    errors: errorReducer
+});
